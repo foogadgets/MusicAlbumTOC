afffc794ec6d3634d808f9564aafaa01efdd1d02
foogadgets <foogadgets@users.noreply.github.com>
2023-12-30 20:30:01 +0100

    Fixed memory leaks. Now checked with valgrind

--- MusicAlbumTOC.cpp
+++ MusicAlbumTOC.cpp
@@ -5,31 +5,60 @@
 
 MusicAlbumTOC::MusicAlbumTOC(void)
 {
+  this->_albumName = (char*)calloc(1, sizeof(char));
+  strncpy(this->_albumName, "\0", 1);
+
+  for (uint8_t i = 0; i < MAXTRACKNO; i++) {
+    this->_trackName[i] = (char*)calloc(1, sizeof(char));
+    strncpy(this->_trackName[i], "\0", 1);
+
+    this->_trackDuration_ms[i] = (uint32_t*)malloc(sizeof(uint32_t));
+    *this->_trackDuration_ms[i] = 0;
+  }
+
+  this->_numberOfTracks = 0;
+  this->_usedBlocks = 0;
 }
 
 
 MusicAlbumTOC::~MusicAlbumTOC(void)
 {
-  clearTOC();
+  if(this->_albumName != NULL) {
+    free(this->_albumName);
+    this->_albumName = NULL;
+  }
+
+  for (uint8_t i = 0; i < MAXTRACKNO; i++) {
+    if (this->_trackName[i] != NULL) {
+      free(this->_trackName[i]);
+      this->_trackName[i] = NULL;
+    }
+    if (this->_trackDuration_ms[i] != NULL) {
+      free(this->_trackDuration_ms[i]);
+    }
+  }
 }
 
 
 uint8_t
 MusicAlbumTOC::setAlbumName(const char *albumName)
 {
-  if(albumName == NULL) { return 4; }	// album a Nullstring is not allowed
+  // Check how many blocks will be consumed after album Name change
+  if ((this->_usedBlocks - (strlen(this->_albumName)+6)/7 + (strlen(albumName)+6)/7) > MAXBLOCKS) { return 1; }
 
-  if(this->_albumName != NULL) { this->_usedBlocks -= (strlen(this->_albumName)+6)/7; free(this->_albumName); }
+  // Remove old album name
+  this->_usedBlocks -= (strlen(this->_albumName)+6)/7;
+  free(this->_albumName);
 
+  // Add new album name
   this->_albumName = (char*)calloc((strlen(albumName)+1), sizeof(char));
-  if (this->_albumName == NULL) { return 3; }
   strncpy(this->_albumName, albumName, strlen(albumName));
 
   this->_usedBlocks += (strlen(albumName)+6)/7;
-  if (this->_usedBlocks > MAXBLOCKS) { return 2; }
 
   return 0;
 }
+
 
    /* track number start from 1 ... 
    duration is stored in milliseconds
@@ -39,15 +68,11 @@ MusicAlbumTOC::setTrackDuration(const uint8_t trackNo, const uint32_t duration)
 {
   if (trackNo == 0 || trackNo > this->_numberOfTracks) { return 1; }
   
-  if(this->_trackDuration_ms[trackNo-1] == NULL) {
-    this->_trackDuration_ms[trackNo-1] = (uint32_t*)malloc(sizeof(uint32_t));
-    if(this->_trackDuration_ms[trackNo-1] == NULL) { return 3; }
-  }
-
   *this->_trackDuration_ms[trackNo-1] = duration;
 
   return 0;
 }
+
 
    /* track number start from 1 ... 
    Return value in milliseconds
@@ -55,8 +80,9 @@ MusicAlbumTOC::setTrackDuration(const uint8_t trackNo, const uint32_t duration)
 uint32_t
 MusicAlbumTOC::getTrackDuration(const uint8_t trackNo)
 {
-  if (trackNo == 0 || trackNo > this->_numberOfTracks) { return 0UL; }
-  return *(this->_trackDuration_ms[trackNo-1]);
+  if (trackNo == 0 || trackNo > this->_numberOfTracks) { return 0; }
+
+  return *this->_trackDuration_ms[trackNo-1];
 }
 
 
@@ -64,16 +90,12 @@ uint8_t
 MusicAlbumTOC::addTrack(const char *trackName)
 {
   if (this->_numberOfTracks >= MAXTRACKNO) { return 1; }
+  if ((this->_usedBlocks + (strlen(trackName)+6)/7) > MAXBLOCKS) { return 2; }
 
-  if (this->_trackName[this->_numberOfTracks] != NULL) {
-    free(this->_trackName[this->_numberOfTracks]);
-  }
+  free(this->_trackName[this->_numberOfTracks]);
 
-  if ((this->_usedBlocks+(strlen(trackName)+6)/7) > MAXBLOCKS) { return 2; }
 
   this->_trackName[this->_numberOfTracks] = (char*)calloc((strlen(trackName)+1), sizeof(char));
-  if (this->_trackName[this->_numberOfTracks] == NULL) { return 3; }
-
   strncpy(this->_trackName[this->_numberOfTracks], trackName, strlen(trackName));
 
   this->_usedBlocks += (strlen(trackName)+6)/7;
@@ -82,22 +104,19 @@ MusicAlbumTOC::addTrack(const char *trackName)
   return 0;
 }
 
+
 uint8_t
 MusicAlbumTOC::setTrackName(const uint8_t trackNo, const char *trackName)
 {
   if (trackNo == 0 || trackNo > this->_numberOfTracks) { return 1; }
 
-  if (this->_trackName[trackNo-1] != NULL) {
-    uint16_t tmpBlocks = _usedBlocks - (strlen(this->_trackName[trackNo-1])+6)/7;
-    if ((tmpBlocks + (strlen(trackName)+6)/7) > MAXBLOCKS) { return 2; }
+  // Check how many blocks will be consumed after track name change
+  if ((this->_usedBlocks - (strlen(this->_trackName[trackNo-1])+6)/7 + (strlen(trackName)+6)/7) > MAXBLOCKS) { return 2; }
 
-    this->_usedBlocks -= (strlen(this->_trackName[trackNo-1])+6)/7;
-    free(this->_trackName[trackNo-1]);
-  }
+  this->_usedBlocks -= (strlen(this->_trackName[trackNo-1])+6)/7;
+  free(this->_trackName[trackNo-1]);
 
-  this->_trackName[trackNo-1] = (char*)calloc((1+strlen(trackName)), sizeof(char));
-  if (this->_trackName[trackNo-1] == NULL) { return 3; }
-
+  this->_trackName[trackNo-1] = (char*)calloc((strlen(trackName)+1), sizeof(char));
   strncpy(this->_trackName[trackNo-1], trackName, strlen(trackName));
 
   this->_usedBlocks += (strlen(trackName)+6)/7;
@@ -105,17 +124,27 @@ MusicAlbumTOC::setTrackName(const uint8_t trackNo, const char *trackName)
   return 0;
 }
 
+
 uint8_t
 MusicAlbumTOC::getNoTracks(void)
 {
   return this->_numberOfTracks;
 }
 
+
 char*
 MusicAlbumTOC::getAlbumName(void)
 {
   return this->_albumName;
 }
+
+
+uint8_t
+MusicAlbumTOC::getUsedBlocks(void)
+{
+  return this->_usedBlocks;
+}
+
 
 char*
 MusicAlbumTOC::getTrackName(const uint8_t trackNo)
@@ -125,18 +154,21 @@ MusicAlbumTOC::getTrackName(const uint8_t trackNo)
   return this->_trackName[(trackNo - 1)];
 }
 
+
 void
 MusicAlbumTOC::clearTOC(void) {
   if(this->_albumName != NULL) {
     free(this->_albumName);
-    this->_albumName = NULL;
+    this->_albumName = (char*)calloc(1, sizeof(char));
+    strncpy(this->_albumName, "\0", 1);
   }
 
-  for (uint8_t i = 0; i < this->_numberOfTracks; i++) {
+  for (uint8_t i = 0; i < MAXTRACKNO; i++) {
     free(this->_trackName[i]);
-    this->_trackName[i] = NULL;
-    free(this->_trackDuration_ms[i]);
-    this->_trackDuration_ms[i] = NULL;
+    this->_trackName[i] = (char*)calloc(1, sizeof(char));
+    strncpy(this->_trackName[i], "\0", 1);
+
+    *this->_trackDuration_ms[i] = 0;
   }
 
   this->_numberOfTracks = 0;
--- MusicAlbumTOC.h
+++ MusicAlbumTOC.h
@@ -19,15 +19,16 @@ class MusicAlbumTOC
     uint8_t setTrackDuration(const uint8_t trackNo, const uint32_t duration);
     uint32_t getTrackDuration(const uint8_t trackNo);
     uint8_t getNoTracks(void);
+	uint8_t getUsedBlocks(void);
     char*   getAlbumName(void);
     char*   getTrackName(const uint8_t trackNo);
     void    clearTOC(void);
   private:
-    uint16_t _usedBlocks = 0;
-    char   *_albumName = NULL;
+    uint8_t _usedBlocks;
+    char   *_albumName;
     char   *_trackName[MAXTRACKNO] = { NULL };
     uint32_t *_trackDuration_ms[MAXTRACKNO] = { NULL };
-    uint8_t _numberOfTracks = 0;
+    uint8_t _numberOfTracks;
 };
 
 #endif // MUSICALBUMTOC_H
